c961e40dc5a3dd2010e9c59399f0aad75bc03112
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-06-20 22:23:46 +0300

    creating lap 01b

---
+++ app.js
@@ -0,0 +1,23 @@
+let userName
+let userhope
+let reagon
+let favFood
+
+userName = prompt("whte is your name?");
+console.log("userName");
+console.log(userName);
+userhope = prompt("how do you spend your time?");
+console.log("userhope");
+console.log(userhope);
+reagon = prompt("where are you from?");
+console.log("reagon");
+console.log(reagon);
+favFood = prompt("whte is your favorite Food?");
+console.log("favFood");
+console.log(favFood);
+
+alert ("well " + userName + "; " + " you are most welcome "
+    + "i'm also from  " + reagon + "  i realy love "
+    + favFood + " and i like " + userhope + " ^_^ ");
+
+
